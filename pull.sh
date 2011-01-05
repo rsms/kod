6bc3b967745f10dd0d3bda58e5d802fbddac5616
aristidesfl <aristidesfl@gmail.com>
2011-01-05 01:10:52 +0100

    option added to pull.sh (upstream) use: ./pull.sh (to pull from origin) ./pull.sh upstream (to pull from upstream (https://github.com/rsms/kod.git))

--- pull.sh
+++ pull.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-git pull && git submodule update --init
+
+if [[ $1 && $1 == upstream ]]; then
+	echo "Pulling from upstream (https://github.com/rsms/kod.git)..."
+	git pull https://github.com/rsms/kod.git 
+else
+	echo "Pulling from origin..."
+	git pull
+fi
+
+echo "Updating submodules..."
+git submodule update --init
