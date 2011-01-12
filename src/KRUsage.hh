7503e9c2d5de5cef8ca9642b5241a5ab821a6558
Reed Stoner <kaltekar@gmail.com>
2011-01-11 22:36:00 -0500

    Fixed merge conflict

--- src/KRUsage.hh
+++ src/KRUsage.hh
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 // Copyright (c) 2010-2011, Rasmus Andersson. All rights reserved.
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
-=======
+
 #ifndef K_RUSAGE_H_
 #define K_RUSAGE_H_
 
 #if KOD_WITH_K_RUSAGE
 
 #include <deque>
->>>>>>> upstream/master
+
 
 /*!
  * Record resource usage using getrusage(2)
