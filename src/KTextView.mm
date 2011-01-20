934afb17b1cca91ce67dfd33689d77766a04e454
Edward Patel <edward.patel@memention.com>
2011-01-21 00:07:25 +0100

    Cleaned the printing with enscript a little. Placed it in a thread. Added a config variable for color usage: printing/useColors.
    This is a hack to be used until someone feels comfortable to make the real printing that will use the built in highlighting.

--- src/KTextView.mm
+++ src/KTextView.mm
@@ -92,6 +92,60 @@ static NSColor *kColumnGuideColor = nil, *kColumnGuideBackgroundColor = nil;
 
 
 #pragma mark -
+#pragma mark Internals
+
+
+// Note(epatel) This is a very *hacked* way to print a buffer with the help of 'enscript'
+// Feel free to fix what ever you think can be improved.
+- (void)printWithEnscript_ {
+  dispatch_async(dispatch_get_main_queue(), ^ { 
+    NSAutoreleasePool *__arpool = [NSAutoreleasePool new]; 
+    NSString *tmpPath = [NSString stringWithFormat:@"/tmp/kod-printing-%@", self.document.displayName];
+    NSString *tmpPathPS = [NSString stringWithFormat:@"%@.ps", tmpPath];
+    NSString *enscriptCommand = [NSString stringWithFormat:@"enscript %@ %@ %@ %@ %@ %@ %@ %@ %@ %@ %@ %@",
+                                 @"--newline=n",
+                                 @"--tabsize=4",
+                                 @"--encoding=mac",
+                                 @"--highlight",
+                                 @"--borders",
+                                 @"--landscape",
+                                 @"--columns=2",
+                                 @"--silent",
+                                 [NSString stringWithFormat:@"--header='|%@|$D $C'", self.document.displayName],
+                                 [NSString stringWithFormat:@"--color=%d", kconf_bool(@"printing/useColors", YES) ? 1 : 0],
+                                 [NSString stringWithFormat:@"-o \"%@\"", tmpPathPS], // Out file
+                                 [NSString stringWithFormat:@"\"%@\"", tmpPath]]; // In file
+    NSString *deleteCommand = [NSString stringWithFormat:@"rm /tmp/kod-printing-*"]; // Clean any previous print files
+    NSString *openCommand = [NSString stringWithFormat:@"open \"%@\"", tmpPathPS];
+    
+    system([deleteCommand cStringUsingEncoding:NSUTF8StringEncoding]);
+
+    // Should pass buffer to stdin of enscript process...but that will need to pass content language to enscript with
+    // --highlight=LANG
+    NSData *data = [self.string dataUsingEncoding:NSMacOSRomanStringEncoding 
+                             allowLossyConversion:YES];
+    if ([data writeToFile:tmpPath atomically:NO] == NO) {
+      NSLog(@"Failed to write temp file: %@", tmpPath);
+    } else {
+      system([enscriptCommand cStringUsingEncoding:NSUTF8StringEncoding]);
+      system([openCommand cStringUsingEncoding:NSUTF8StringEncoding]);
+    }
+        
+    [__arpool drain]; 
+  });
+}
+
+
+#pragma mark -
+#pragma mark Printing
+
+
+- (void)print:(id)sender {
+  [self printWithEnscript_];
+}
+
+
+#pragma mark -
 #pragma mark Drawing
 
 
@@ -124,26 +178,6 @@ static NSColor *kColumnGuideColor = nil, *kColumnGuideBackgroundColor = nil;
       [NSBezierPath strokeLineFromPoint:srcPoint toPoint:dstPoint];
     }
   }
-}
-
-
-#pragma mark -
-#pragma mark Printing
-
-
-- (void)print:(id)sender {
-  NSString *tmpPath = [NSString stringWithFormat:@"/tmp/%@", self.document.displayName];
-  NSString *tmpPathPS = [NSString stringWithFormat:@"%@.ps", tmpPath];
-  NSString *enscriptCommand = [NSString stringWithFormat:@"enscript --newline=n --tabsize=4 -X mac --header='|$n|$D $C' -E -jr -2 -o \"%@\" \"%@\"", tmpPathPS, tmpPath];
-  NSString *deleteCommand = [NSString stringWithFormat:@"rm \"%@\"", tmpPathPS];
-  NSString *openCommand = [NSString stringWithFormat:@"open \"%@\"", tmpPathPS];
-  [self.string writeToFile:tmpPath
-                atomically:NO
-                  encoding:NSMacOSRomanStringEncoding // enscript doesn't support UTF8...yet
-                     error:nil];
-  system([deleteCommand cStringUsingEncoding:NSUTF8StringEncoding]);
-  system([enscriptCommand cStringUsingEncoding:NSUTF8StringEncoding]);
-  system([openCommand cStringUsingEncoding:NSUTF8StringEncoding]);
 }
 
 
