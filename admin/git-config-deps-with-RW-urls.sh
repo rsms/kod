db829ab893108cc158f9499655070bc5a0b7ecd2
Rasmus Andersson <rasmus@notion.se>
2011-01-31 11:45:59 +0100

    added deps/gazelle

--- admin/git-config-deps-with-RW-urls.sh
+++ admin/git-config-deps-with-RW-urls.sh
@@ -2,6 +2,7 @@
 (cd deps/libcss && git remote set-url origin git@github.com:rsms/libcss-osx.git)
 (cd deps/hunch-cocoa && git remote set-url origin git@github.com:rsms/hunch-cocoa.git)
 (cd deps/chromium-tabs && git remote set-url origin git@github.com:rsms/chromium-tabs.git)
+(cd deps/gazelle && git remote set-url origin git@github.com:rsms/gazelle.git)
 
 echo Submodule upstream URLs are now configured as:
 git submodule foreach git remote -v
